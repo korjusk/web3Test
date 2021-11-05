f54e4a63dee7fc8038ffd57ef0a296de076a301e
Kaur Korjus <korjusk@gmail.com>
2021-11-05 14:14:08 +0200

    Updates

---
+++ precache-manifest.d96d58bc0d4d6853b682e664c24ffb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730a399ae061e5619908dbc9d1f7f06c",
+    "url": "/web3Test/index.html"
+  },
+  {
+    "revision": "62dd5664a671024a2c8f",
+    "url": "/web3Test/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "62dd5664a671024a2c8f",
+    "url": "/web3Test/static/js/2.822093b5.chunk.js"
+  },
+  {
+    "revision": "c0713bc4934e1b0aca8afcb2b1757c19",
+    "url": "/web3Test/static/js/2.822093b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02547e8d5a074f8b7962",
+    "url": "/web3Test/static/js/main.e68a7910.chunk.js"
+  },
+  {
+    "revision": "4881657e29c94db44cad",
+    "url": "/web3Test/static/js/runtime-main.a20e41d7.js"
+  }
+]);
